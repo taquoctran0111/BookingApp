f3e387404c7929c899fe8db7de22ce0ee76f89dc
taquoctran <ta.quoctran@tap-ic.co.jp>
2022-02-12 08:25:57 +0700

    update

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,33 +1,38 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
 import './header.css';
 import logo from '../../assets/image/logo.png';
 
-const url = "http://localhost:8797/hotels";
+// const url = "http://localhost:8797/hotels";
 function Header() {
     // const [hotel, setHotel] = useState([]);
 
-    useEffect(() => {
-        fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => console.error(error))
-    }, []);
+    // useEffect(() => {
+    //     fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //     })
+    //     .catch((error) => console.error(error))
+    // }, []);
     // console.log('test1');
     // console.log(hotel);
     return (
-        <div className='header'>
-            <div className = "logo-area">
-                <img src = {logo} className = "logo-icon"/>
-                <span className='logo'>Booking</span>
+        <div className="header-form">
+            <div className = "header">
+                <div className = "logo-area">
+                    <img src = {logo} className = "logo-icon"/>
+                    <span className='logo'>Booking</span>
+                </div>
+                <div style={{display: "flex"}}>
+                    <div className="button">Sign in</div>
+                    <div className="button">Sign up</div>
+                </div>
             </div>
-            <div style={{display: 'flex'}}>
-                <div className='button'>Sign in</div>
-                <div className='button'>Sign up</div>
+            <div className = "menu">
+                <div>Menu bar</div>
             </div>
-        </div>
+        </div>   
     );
 }
 
